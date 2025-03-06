bc0fd758790ef100a2f6cc18e7ef591b8b862cce
ElLocoBill <roamarillas3@gmail.com>
2025-03-05 20:40:13 -0700

    Aprendizaje

--- src/clases/tareasItson/EstadosSemaforo.java
+++ src/clases/tareasItson/EstadosSemaforo.java
@@ -1,15 +1,12 @@
 package tareasItson;
 
-import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class EstadosSemaforo {
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
         String[] estados = new String[3];
         String[] mensajes = new String[3];
-        String peticion = "Porfavor introduzca el estado del semaforo que desea ver... ";
-        String obtener;
 
         estados[0] = "--Verde--\n";
         mensajes[0] = "El semaforo esta en verde, puedes pasar!!";
@@ -20,17 +17,26 @@ public class EstadosSemaforo {
         estados[2] = "--Rojo--\n";
         mensajes[2] = "El semaforo esta en Rojo, Detente!!";
 
-        System.out.println(peticion);
-        obtener = s.nextLine();
+        try{
+            for (int i = 0; i <= estados.length; i++){
+                System.out.println("\n" + estados[i].concat(mensajes[i]));
 
-        if(obtener.equalsIgnoreCase("verde")){
-            System.out.println(estados[0].concat(mensajes[0]));
-
-        } else if (obtener.equalsIgnoreCase("amarillo")) {
-            System.out.println(estados[1].concat(mensajes[1]));
-
-        } else if (obtener.equalsIgnoreCase("rojo")) {
-            System.out.println(estados[0].concat(mensajes[2]));
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("");
+            System.out.println("El programa a terminado.");
         }
+
+
+
+//        if(obtener.equalsIgnoreCase("verde")){
+//            System.out.println(estados[0].concat(mensajes[0]));
+//
+//        } else if (obtener.equalsIgnoreCase("amarillo")) {
+//            System.out.println(estados[1].concat(mensajes[1]));
+//
+//        } else if (obtener.equalsIgnoreCase("rojo")) {
+//            System.out.println(estados[0].concat(mensajes[2]));
+//        }
     }
 }
